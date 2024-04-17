93a2589745412a8501088294f46183dd34595ab8
Nitheshwaran <nitheshwaran003@gmail.com>
2024-04-17 15:37:19 +0530

    toala

--- client/src/components/Invoice.jsx
+++ client/src/components/Invoice.jsx
@@ -160,6 +160,17 @@ const Invoice = ({
         return unitRate;
     }
 
+    function formatAmountToIndianCurrency(amount) {
+        var formattedAmount;
+        if (amount) {
+            var formattedAmount = amount.toLocaleString('en-IN', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        } else {
+            var formattedAmount = "";
+        }
+
+        return formattedAmount;
+    }
+
 
 
     function TaxableValue() {
@@ -579,10 +590,10 @@ const Invoice = ({
     }
 
     // After round off config
-    const GrandTotalCGST = () =>{
+    const GrandTotalCGST = () => {
         return Math.floor(TotalcgstValue() * 100) / 100
     }
-    const GrandTotalSGST = () =>{
+    const GrandTotalSGST = () => {
         return Math.floor(TotalsgstValue() * 100) / 100
     }
 
